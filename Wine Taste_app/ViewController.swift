dee508a74ec3a66e31ec4f3a29fd78583a192a98
Testing_author <testing_beta@mail.com>
2020-10-19 23:58:27 +0300

    Initial Commit

---
+++ Wine Taste_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wine Taste_app
+//
+//  Created by DDDD on 19/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
